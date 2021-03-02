6cb2c475bdfd822b2814b991cbe093d10864522a
benjicwood <bencharlwood@gmail.com>
2021-03-02 10:33:16 +0000

    2022 lets goo

--- src/app/downloadfestival/downloadfestival.component.ts
+++ src/app/downloadfestival/downloadfestival.component.ts
@@ -20,9 +20,9 @@ export class DownloadfestivalComponent {
   public thirdStage = thirdStageList
   public fourthStage = fourthStageList
 
-  public fridayHeadliner: string
-  public saturdayHeadliner: string
-  public sundayHeadliner: string
+  public fridayHeadliner: string = 'kiss'
+  public saturdayHeadliner: string = 'ironmaiden'
+  public sundayHeadliner: string = 'biffyclyro'
   public fridaySub1: string
   public saturdaySub1: string
   public sundaySub1: string
